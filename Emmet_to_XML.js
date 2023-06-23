6b494f5782dcb7eb864dcde3bed88f41f77c8195
Lachezar Tsvetkov <lachezardts@gmail.com>
2023-06-23 23:51:25 +0300

    Finalized the conversion between Emmet and XML

--- Emmet_to_XML.js
+++ Emmet_to_XML.js
@@ -2,61 +2,147 @@ function EmmetToXML(input)
 {
     let output = "";
 
-    let tags = [];
+    let tags = [[]];
     let currentTag = "";
 
+    let currentGroup = 0;
+    let groupCount = [];
+    groupCount[0] = 0;
+    let wasGrouped = false;
+
+    let attributes = "";
+    let textValue = "";
+
     let currentLevel = 1;
+
+    
 
     for (let i = 0; i < input.length; i++) 
     {
         let char = input[i];
-        if (char === ">") 
+        if (char == ">") 
         {
-            tags[currentLevel - 1] = currentTag;
+            if(wasGrouped)
+            {
+                wasGrouped = false;
+                continue;
+            }
+            groupCount[currentGroup]++;
+            tags[currentGroup][currentLevel - 1] = currentTag;
+            
+            output += "\n" + "\t".repeat(currentLevel) + "<" + currentTag + attributes + ">" + textValue;
 
-            output += "\n" + "\t".repeat(currentLevel) + "<" + tags[currentLevel - 1] + ">";
-
-            currentLevel++;
             currentTag = "";
+            attributes = "";
+            textValue = "";
+            currentLevel++;
         }
         else if (char == "+") 
         {
-            tags[currentLevel - 1] = currentTag;
-            output += "\n" + "\t".repeat(currentLevel) + "<" + tags[currentLevel - 1] + "></" + tags[currentLevel - 1] + ">";
+            if(wasGrouped)
+            {
+                wasGrouped = false;
+                continue;
+            }
+            
+            tags[currentGroup][currentLevel - 1] = currentTag;
+            output += "\n" + "\t".repeat(currentLevel) + "<" + tags[currentGroup][currentLevel - 1] + attributes + ">" + textValue + "</" + tags[currentGroup][currentLevel - 1] + ">";
+            
             currentTag = "";
+            attributes = "";
+            textValue = "";
         }
         else if (char == "^") 
         {
-            if (currentTag != "") 
+            if (currentTag=="")
             {
-                tags[currentLevel - 1] = currentTag;
-                output += "\n" + "\t".repeat(currentLevel) + "<" + tags[currentLevel - 1] + "></" + tags[currentLevel - 1] + ">";
+                currentLevel--;
+                output += "\n" + "\t".repeat(currentLevel) + "</" + tags[currentGroup][currentLevel - 1] + ">";
+            }
+            else
+            {
+                tags[currentGroup][currentLevel - 1] = currentTag;
+                output += "\n" + "\t".repeat(currentLevel) + "<" + tags[currentGroup][currentLevel - 1] + attributes + ">" + textValue + "</" + tags[currentGroup][currentLevel - 1] + ">";
 
                 currentLevel--;
-                output += "\n" + "\t".repeat(currentLevel) + "</" + tags[currentLevel - 1] + ">";
+
+                output += "\n" + "\t".repeat(currentLevel) + "</" + tags[currentGroup][currentLevel - 1] + ">";
+
                 currentTag = "";
-            }
-            else 
-            {
-                currentLevel--;
-                output += "\n" + "\t".repeat(currentLevel) + "</" + tags[currentLevel - 1] + ">";
+                attributes = "";
+                textValue = "";
             }
         }
         else if (char == "{")
         {
+            i++;
+            char = input[i];
+            while (char != "}")
+            {
+                textValue += char;
 
-        }
-        else if (char == "}")
-        {
-
+                i++;
+                char = input[i];
+            }
+            console.log(textValue);
         }
         else if (char == "(")
         {
+            currentGroup++;
+            groupCount[currentGroup] = 1;
 
+            if(currentGroup >= tags.length)
+            {
+                tags.push([]);
+            }
         }
         else if (char == ")")
         {
+            output += "\n" + "\t".repeat(currentLevel) + "<" + currentTag + attributes + ">" + textValue + "</" + currentTag + ">";
+            for( let j = 0; j < groupCount[currentGroup] - 1; j++)
+            {
+                currentLevel--;
+                output += "\n" + "\t".repeat(currentLevel) + "</" + tags[currentGroup][currentLevel - 1] + ">";
+            }
+            currentGroup--;
             
+            currentTag = "";
+            attributes = "";
+            textValue = "";
+
+            wasGrouped = true;
+        }
+        else if (char == "[")
+        {
+            console.log("starting");
+            let hasValue = false;
+            let inString = false;
+
+            i++;
+            char = input[i];
+            while (char != "]")
+            {
+                if (char == "\"" && !inString) inString = true;
+                else if (char=="\"" && inString) inString = false;
+                if (char == "=")
+                {
+                    hasValue = true;
+                    attributes += char;
+                }
+                else if (hasValue && char == " " && !inString)
+                {
+                    hasValue = false;
+                    attributes += " ";
+                }
+                else if (!hasValue && char == " " && !inString) attributes += '="" ';
+                else if (!hasValue && input[i + 1] == "]") 
+                    attributes += char + '=""';
+                else attributes += char;
+
+                i++;
+                char = input[i];
+            }
+            attributes = " " + attributes;
         }
         else 
         {
@@ -64,12 +150,14 @@ function EmmetToXML(input)
         }
     }
 
-    tags[currentLevel - 1] = currentTag;
-    output += "\n" + "\t".repeat(currentLevel) + "<" + currentTag + ">";
+    tags[currentGroup][currentLevel - 1] = currentTag;
+    output += "\n" + "\t".repeat(currentLevel) + "<" + currentTag + attributes + ">" + textValue;
 
     for (let i = 0; currentLevel > i; currentLevel--) {
-        output += "</" + tags[currentLevel - 1] + ">\n" + "\t".repeat(currentLevel - 1);
+        output += "</" + tags[currentGroup][currentLevel - 1] + ">\n" + "\t".repeat(currentLevel - 1);
     }
+
+    console.log(output);
 
     return output;
 }
